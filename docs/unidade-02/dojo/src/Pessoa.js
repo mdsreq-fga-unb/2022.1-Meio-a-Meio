513340967f19fcd931f0648ecbd530d2a9cf0959
Bruna Lima <bruna.b.santos@capgemini.com>
2022-07-17 22:35:11 -0300

    organização das pastas

---
+++ docs/unidade-02/dojo/src/Pessoa.js
@@ -0,0 +1,11 @@
+// Classe pessoa para o desafio do coding dojo
+
+class Pessoa {
+    constructor(nome, idade, altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+}
+
+module.exports = Pessoa;
