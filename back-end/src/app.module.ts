707c83418523c0fecc0e2ec71aea2529c053d7f2
Giulia <alcantaragiubs@gmail.com>
2022-08-05 08:45:23 -0300

    feat(#): organizing the code

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,9 @@
-import { ProfessorModule } from './professor/professor.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { User } from './entitys/user.entity';
+import { ProfessorModule } from './model/professor/professor.module';
 import { StudentModule } from './model/student/student.module';
 
 @Module({
