bc864e1fcaab223bb1bce7fc5f931e72dbef5c84
Giulia <alcantaragiubs@gmail.com>
2022-08-05 08:19:05 -0300

    #(feature) dB changes

--- back-end/src/model/database/database.providers.ts
+++ back-end/src/model/database/database.providers.ts
@@ -10,7 +10,7 @@ export const databaseProviders = [
         port: 3306,
         username: 'root',
         password: '30317512',
-        database: 'alunodb',
+        database: 'meioameio',
         // database: 'dashcommerce_data_test',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true, //toda vez que rodar a aplicação, o nest vai tentar sincronizar o db com as classes
