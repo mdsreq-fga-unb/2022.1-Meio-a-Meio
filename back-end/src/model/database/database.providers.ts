3bd3b8eac900db698e75eb6457811ed56a49e07d
ciro-c <ciro.fga@gmail.com>
2022-08-16 01:21:45 -0300

    hotfix(#): fix connection db
    Co-Authored-By: Bruna Lima <lbruna886@gmail.com>

--- back-end/src/model/database/database.providers.ts
+++ back-end/src/model/database/database.providers.ts
@@ -2,18 +2,20 @@
 import { Disciplina } from 'src/disciplina/entities/disciplina.entity';
 import { Turma } from 'src/turma/entities/turma.entity';
 import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
 
+config();
 export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: process.env.HOST,
-        port: parseInt(process.env.PORT),
-        username: process.env.USERNAME_DB,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true, //toda vez que rodar a aplicação, o nest vai tentar sincronizar o db com as classes
       });
