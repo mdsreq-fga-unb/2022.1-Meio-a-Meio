5001fde7f5211ef2d8ca62d47b63affc62f27c49
Giulia <alcantaragiubs@gmail.com>
2022-08-17 14:17:47 -0300

    feat(#)Arrumando as telas

--- front-end/pages/docente/portal.tsx
+++ front-end/pages/docente/portal.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 
