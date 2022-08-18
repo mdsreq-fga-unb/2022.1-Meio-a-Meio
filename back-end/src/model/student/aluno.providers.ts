2e93b3cf7694c416fceb59cda5a6dab8e5195b13
ciro-c <ciro.fga@gmail.com>
2022-08-18 00:17:57 -0300

    hotfix(#): fixed merge remains˜

--- back-end/src/model/student/aluno.providers.ts
+++ back-end/src/model/student/aluno.providers.ts
@@ -4,12 +4,8 @@ import { Aluno } from './aluno.entity';
 export const alunoProviders = [
   {
     provide: 'STUDENT_REPOSITORY',
-<<<<<<< HEAD:back-end/src/model/student/aluno.providers.ts
     useFactory: (dataSource: DataSource) => dataSource.getRepository(Aluno),
     inject: ['DATA_SOURCE'], //vem do database.providers
-=======
-    useFactory: (dataSource: DataSource) => dataSource.getRepository(Student),
-    inject: ['DATA_SOURCE'],
->>>>>>> a49e8f09c5617ef9bd017736995f41635916431a:back-end/src/student/student.providers.ts
+
   },
 ];
