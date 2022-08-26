df369b9025111427e103acc120a2e7c1ee7152e6
Guilherme Dib <dibclash@gmail.com>
2022-08-26 11:05:08 -0300

    feat(#)Adding uf changes

--- front-end/pages/professor/cadastro.tsx
+++ front-end/pages/professor/cadastro.tsx
@@ -51,10 +51,24 @@ export default function Cadastro() {
   const [gender, setGender] = useState('');
   const [data, setData] = useState<any>({});
   const [errors , setErrors] = useState<any>({});
-  const [regiao , setRegiao] = useState('');
+  const [region , setRegion] = useState('');
+  const [ufRegion , setUfRegion] = useState('');
+
 
   const handleChange = (event: SelectChangeEvent) => {
     setGender(event.target.value);
+  };
+
+  const handleRegion = (event: SelectChangeEvent) => {
+    setRegion(event.target.value);
+    let tempErrors = errors
+    setErrors(tempErrors);
+  };
+
+  const handleUfRegion = (event: SelectChangeEvent) => {
+    setUfRegion(event.target.value);
+    let tempErrors = errors
+    setErrors(tempErrors);
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -296,14 +310,36 @@ export default function Cadastro() {
                     labelId="uf_rg_rne"
                     id="uf_rg_rne"
                     label="uf_rg_rne"
-                    value={regiao}
-                    onChange={handleChange}
+                    value={ufRegion}
+                    onChange={handleUfRegion}
                   >
-                    <MenuItem value="">
-                      <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>DF</MenuItem>
-                    <MenuItem value={20}>GO</MenuItem>
+                    <MenuItem value={1}>AC</MenuItem>
+                    <MenuItem value={2}>AL</MenuItem>
+                    <MenuItem value={3}>AP</MenuItem>
+                    <MenuItem value={4}>AM</MenuItem>
+                    <MenuItem value={5}>BA</MenuItem>
+                    <MenuItem value={6}>CE</MenuItem>
+                    <MenuItem value={7}>DF</MenuItem>
+                    <MenuItem value={8}>ES</MenuItem>
+                    <MenuItem value={9}>GO</MenuItem>
+                    <MenuItem value={10}>MA</MenuItem>
+                    <MenuItem value={11}>MT</MenuItem>
+                    <MenuItem value={12}>MS</MenuItem>
+                    <MenuItem value={13}>MG</MenuItem>
+                    <MenuItem value={14}>PA</MenuItem>
+                    <MenuItem value={15}>PB</MenuItem>
+                    <MenuItem value={16}>PR</MenuItem>
+                    <MenuItem value={17}>PE</MenuItem>
+                    <MenuItem value={18}>PI</MenuItem>
+                    <MenuItem value={19}>RJ</MenuItem>
+                    <MenuItem value={20}>RN</MenuItem>
+                    <MenuItem value={21}>RS</MenuItem>
+                    <MenuItem value={22}>RO</MenuItem>
+                    <MenuItem value={23}>RR</MenuItem>
+                    <MenuItem value={24}>SC</MenuItem>
+                    <MenuItem value={25}>SP</MenuItem>
+                    <MenuItem value={26}>SE</MenuItem>
+                    <MenuItem value={26}>TO</MenuItem>
                   </Select>
                 </FormControl>
               </Grid>
@@ -345,11 +381,9 @@ export default function Cadastro() {
                     labelId="uf_crm"
                     id="uf_crm"
                     label="uf_crm"
-                    value={regiao}
-                    onChange={handleChange}
+                    value={region}
+                    onChange={handleRegion}
                   >
-                    <MenuItem value="">
-                    </MenuItem>
                     <MenuItem value={1}>AC</MenuItem>
                     <MenuItem value={2}>AL</MenuItem>
                     <MenuItem value={3}>AP</MenuItem>
@@ -396,7 +430,7 @@ export default function Cadastro() {
               </Grid>
               <Grid item xs={1.8}> 
                 <FormControl fullWidth>
-                <FormLabel id="especialista">Especialista?</FormLabel>
+                <FormLabel id="especialista" required>Especialista?</FormLabel>
                 <RadioGroup
                   row
                   aria-labelledby="demo-row-radio-buttons-group-label"
@@ -424,7 +458,7 @@ export default function Cadastro() {
               </Grid>
               <Grid item xs={2.5}> 
               <FormControl fullWidth>
-                <FormLabel id="sexo">Sexo</FormLabel>
+                <FormLabel id="sexo" required>Sexo</FormLabel>
                 <RadioGroup
                   row
                   aria-labelledby="demo-row-radio-buttons-group-label"
