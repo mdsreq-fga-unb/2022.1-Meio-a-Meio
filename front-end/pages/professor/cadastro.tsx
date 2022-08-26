f0f7d0aa19baf72ac39a15d3515ce7b409d3cc31
Guilherme Dib <dibclash@gmail.com>
2022-08-26 10:38:14 -0300

    feat(#): Changing uf fields

--- front-end/pages/professor/cadastro.tsx
+++ front-end/pages/professor/cadastro.tsx
@@ -1,4 +1,4 @@
-import React, {useState, ChangeEvent} from 'react';
+import React, {useState, ChangeEvent, FunctionComponentElement} from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -23,8 +23,6 @@ import NativeSelect from '@mui/material/NativeSelect';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import apiRequest from '../../util/apiRequest';
-import FormHelperText from '@mui/material/FormHelperText';
-
 
 
 function Copyright(props: any) {
@@ -46,20 +44,14 @@ const theme = createTheme();
 export default function Cadastro() {
   const [value, setValue] = useState<Date | null>(null);
   const [gender, setGender] = useState('');
-  const [boolean, setBoolean] = useState('');
   const [data, setData] = useState<any>({});
   const [errors , setErrors] = useState<any>({});
+  const [regiao , setRegiao] = useState('');
 
   const handleChange = (event: SelectChangeEvent) => {
     setGender(event.target.value);
-    let tempErrors = errors
-    setErrors(tempErrors);
   };
-  const handleBoolean = (event: SelectChangeEvent) => {
-    setBoolean(event.target.value);
-    let tempErrors = errors
-    setErrors(tempErrors);
-  };
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if(handleCheckData()){
@@ -118,8 +110,7 @@ export default function Cadastro() {
       uf_crm,
       especializacao,
       formacao_academica,
-      email,
-      especialista,
+ 
     } = data;
     let emptyFields: any = {}
 
@@ -159,12 +150,10 @@ export default function Cadastro() {
     if(!formacao_academica || formacao_academica.length === 0) {
       emptyFields.formacao_academica = "O campo de formação acadêmica não pode ser vazio"
     }
-    if(!email || email.length === 0) {
-      emptyFields.email = "O campo de email não pode ser vazio"
-    }
     if(Object.keys(emptyFields).length > 0){
       setErrors(emptyFields);
       return 1;
+      
     }
     return 0;
   }  
@@ -295,22 +284,23 @@ export default function Cadastro() {
                   value= {data?data.rg_rne:""}
                 />
               </Grid>
-              <Grid item xs={1}>
-                <TextField
-                  required
-                  inputProps={{
-                    maxLength: 2,
-                  }}
-                  error={errors.uf_rg_rne?true:false}
-                  helperText={errors.uf_rg_rne||null}
-                  fullWidth
-                  name="uf_rg_rne"
-                  label="UF"
-                  type="uf_rg_rne"
-                  id="uf_rg_rne"
-                  onChange={handleText}
-                  value= {data?data.uf_rg_rne:""}
-                />
+              <Grid item xs={1}> 
+                <FormControl fullWidth>
+                <InputLabel id="uf_rg_rne" required>UF</InputLabel>
+                  <Select
+                    labelId="uf_rg_rne"
+                    id="uf_rg_rne"
+                    label="uf_rg_rne"
+                    value={regiao}
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>DF</MenuItem>
+                    <MenuItem value={20}>GO</MenuItem>
+                  </Select>
+                </FormControl>
               </Grid>
               <Grid item xs={2}>
                 <TextField
@@ -343,21 +333,47 @@ export default function Cadastro() {
                   value= {data?data.crm:""}
                 />
               </Grid>
-              <Grid item xs={1}>
-                <TextField
-                  required
-                  inputProps={{
-                    maxLength: 2,
-                  }}
-                  error={errors.uf_crm?true:false}
-                  helperText={errors.uf_crm||null}
-                  fullWidth
-                  name="uf_crm"
-                  label="UF"
-                  type="uf_crm"
-                  id="uf_crm"
-                 
-                />
+              <Grid item xs={1}> 
+                <FormControl fullWidth>
+                <InputLabel id="uf_crm" required>UF</InputLabel>
+                  <Select
+                    labelId="uf_crm"
+                    id="uf_crm"
+                    label="uf_crm"
+                    value={regiao}
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="">
+                    </MenuItem>
+                    <MenuItem value={1}>AC</MenuItem>
+                    <MenuItem value={2}>AL</MenuItem>
+                    <MenuItem value={3}>AP</MenuItem>
+                    <MenuItem value={4}>AM</MenuItem>
+                    <MenuItem value={5}>BA</MenuItem>
+                    <MenuItem value={6}>CE</MenuItem>
+                    <MenuItem value={7}>DF</MenuItem>
+                    <MenuItem value={8}>ES</MenuItem>
+                    <MenuItem value={9}>GO</MenuItem>
+                    <MenuItem value={10}>MA</MenuItem>
+                    <MenuItem value={11}>MT</MenuItem>
+                    <MenuItem value={12}>MS</MenuItem>
+                    <MenuItem value={13}>MG</MenuItem>
+                    <MenuItem value={14}>PA</MenuItem>
+                    <MenuItem value={15}>PB</MenuItem>
+                    <MenuItem value={16}>PR</MenuItem>
+                    <MenuItem value={17}>PE</MenuItem>
+                    <MenuItem value={18}>PI</MenuItem>
+                    <MenuItem value={19}>RJ</MenuItem>
+                    <MenuItem value={20}>RN</MenuItem>
+                    <MenuItem value={21}>RS</MenuItem>
+                    <MenuItem value={22}>RO</MenuItem>
+                    <MenuItem value={23}>RR</MenuItem>
+                    <MenuItem value={24}>SC</MenuItem>
+                    <MenuItem value={25}>SP</MenuItem>
+                    <MenuItem value={26}>SE</MenuItem>
+                    <MenuItem value={26}>TO</MenuItem>
+                  </Select>
+                </FormControl>
               </Grid>
               <Grid item xs={4}>
                 <TextField
@@ -380,14 +396,14 @@ export default function Cadastro() {
                     labelId="especialista"
                     id="especialista"
                     label="Especialista"
-                    value={boolean}
-                    onChange={handleBoolean}
+                    value={gender}
+                    onChange={handleChange}
                   >
                     <MenuItem value="">
                       <em>None</em>
                     </MenuItem>
-                    <MenuItem value={10}>Sim</MenuItem>
-                    <MenuItem value={20}>Não</MenuItem>
+                    <MenuItem value={90}>Sim</MenuItem>
+                    <MenuItem value={80}>Não</MenuItem>
                   </Select>
                 </FormControl>
               </Grid>
@@ -418,26 +434,13 @@ export default function Cadastro() {
                     <MenuItem value="">
                       <em>None</em>
                     </MenuItem>
-                    <MenuItem value={1}>Homem</MenuItem>
-                    <MenuItem value={2}>Mulher</MenuItem>
+                    <MenuItem value={10}>Homem</MenuItem>
+                    <MenuItem value={20}>Mulher</MenuItem>
                   </Select>
                 </FormControl>
               </Grid>
-              <Grid item xs={6}>
-                <TextField
-                  required
-                  error={errors.email?true:false}
-                  helperText={errors.email||null}
-                  fullWidth
-                  name="email"
-                  label="Email"
-                  type="email"
-                  id="email"
-                  onChange={handleText}
-                  value= {data?data.email:""}
-                />
-				      </Grid>
-              <Grid item xs={6}>
+
+              <Grid item xs={12}>
                 <TextField
                   fullWidth
                   name="observacoes"
