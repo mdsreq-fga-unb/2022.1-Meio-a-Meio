284f04632e00e4b74d72b769f0b0c09c3be2fd57
samuel <samuelsato2009@gmail.com>
2022-08-30 20:51:46 -0300

    hotfix: update database.providers

--- back-end/src/database/database.providers.ts
+++ back-end/src/database/database.providers.ts
@@ -21,7 +21,7 @@ export const databaseProviders = [
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
-        entities: [Aluno, Disciplina, Professor, Curso, Turma, Endereco],
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       });
 
