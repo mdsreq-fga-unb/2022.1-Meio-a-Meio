3f9136b1d2491c3e9b18ecb1d72889b171f4ecde
Bruna Lima <lbruna886@gmail.com>
2022-08-31 11:04:52 -0300

    feat(#): fix import name

--- back-end/src/aluno/aluno.module.ts
+++ back-end/src/aluno/aluno.module.ts
@@ -6,8 +6,6 @@ import { DatabaseModule } from '../database/database.module';
 import { AlunoController } from './aluno.controller';
 import { alunoProviders } from './aluno.providers';
 import { AlunoService } from './aluno.service';
-import { Endereco } from 'src/endereco/endereco.entity';
-import { enderecoProviders } from 'src/endereco/endereco.providers';
 
 @Module({
   imports: [DatabaseModule, EnderecoModule],
